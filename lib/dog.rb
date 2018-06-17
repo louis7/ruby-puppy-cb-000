b9149997882223c0dd4f6524d978a228da850118
louis baptiste <louisvis@ymail.com>
2018-06-17 03:39:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,18 @@
 
 
 class Dog
-  @@all= []
+  @@all = []
  attr_accessor :name
  def initializes(name)
     @name = name
-    @@all<<self
+    @@all << self
  end
 
-
+ 
+  def self.alll
+    @@all.each do |item|
+      puts item.name
+    end
+  end
+  
 end
